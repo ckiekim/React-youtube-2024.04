054811943743eb145d95a3cb892f1ca6e2f0276e
Cheolkie Kim <ckiekim@naver.com>
2024-04-11 17:16:49 +0900

    debug

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
@@ -22,28 +23,38 @@ export default function SearchHeader() {
   }, [keyword]);
 
   return (
-    <Stack direction={'row'} sx={{alignItems: 'center'}}>
-      <Link to='/'>
-        <Stack direction={'row'} spacing={1}>
-          <YouTubeIcon color='error' fontSize="large" />
-          <Typography variant="h5" color='error' sx={{fontWeight: 'bold'}}>Youtube</Typography>
-        </Stack>
-      </Link>
-      <Paper
-        component="form" onClick={handleSubmit}
-        sx={{ p:'2px 4px', display:'flex', alignItems:'center', width:400, marginLeft:15 }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="검색..."
-          value={text}
-          onChange={e => setText(e.target.value)}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton type="button" sx={{ p: 1 }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-    </Stack>
+    <header>
+      <Stack direction={'row'} sx={{alignItems: 'center'}}>
+        <Grid container>
+          <Grid item xs={3}>
+            <Link to='/' style={{textDecoration: 'none'}}>
+              <Stack direction={'row'} spacing={1}>
+                <YouTubeIcon color='error' fontSize="large" />
+                <Typography variant="h4" color='error' sx={{fontWeight: 'bold'}}>Youtube</Typography>
+              </Stack>
+            </Link>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper
+              component="form" onClick={handleSubmit}
+              sx={{ p:'2px 4px', display:'flex', alignItems:'center', width:'100%' }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="검색..."
+                value={text} 
+                onChange={e => setText(e.target.value)}
+              />
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+              <IconButton type="button" sx={{ p: 1 }} aria-label="search" onClick={handleSubmit}>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
+      </Stack>
+      <Divider sx={{my: 1}} />
+    </header>
   )
 }
