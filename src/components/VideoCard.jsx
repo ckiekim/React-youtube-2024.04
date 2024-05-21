af9d252eeab735a65a3b0b21748770b333f64998
Cheolkie Kim <ckiekim@naver.com>
2024-05-21 12:14:26 +0900

    refactoring

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -15,9 +16,16 @@ export default function VideoCard({ video }) {
   return (
     <Card 
       onClick={() => { navigate(`/videos/watch/${videoId}`, {state: {video} }) }}
+      sx={{ maxWidth: 345,  height: '100%', display: 'flex', 
+          flexDirection: 'column', justifyContent: 'space-between',
+          "&:hover": { transform: 'scale(1.05)', transition: 'transform 0.3s' } }}
     >
-      <CardContent>
-        <img src={thumbnails.medium.url} alt={title} />
+      <CardContent 
+        sx={{ flexGrow: 1, display: 'flex',  
+          flexDirection: 'column', justifyContent: 'flex-start' }}
+      >
+        <Box component="img" src={thumbnails.medium.url} alt={title} />
+        {/* <img src={thumbnails.medium.url} alt={title} /> */}
         <div>
           <Typography sx={{fontSize: 16, fontWeight: 'bold'}}>{title}</Typography>
           <Typography>{channelTitle}</Typography>
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
@@ -12,11 +13,11 @@ export default function Videos() {
   const { isLoading, error, videos } = useVideo(keyword);
 
   return (
-    <>
+    <Container mt={5}>
       {isLoading && <p><HourglassTopIcon /> Loading...</p>}
       {error && <p><WarningAmberIcon /> Something is wrong!!!</p>}
       {videos && (
-        <Grid container spacing={1}>
+        <Grid container spacing={3}>
           {videos.map(video => (
             <Grid item xs={12} sm={6} md={4} lg={3}>
               <VideoCard video={video} key={video.id} />
@@ -24,6 +25,6 @@ export default function Videos() {
           ))}
         </Grid>
       )}
-    </>
+    </Container>
   )
 }
